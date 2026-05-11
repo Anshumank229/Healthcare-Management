e3ee62a6f0c16df2da45b66aefff8fccfc8a93f4
Anshuman Kumar <anshuman229@gmail.com>
2026-05-11 22:23:35 +0530

    fix: move docker files to project root

--- main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-app=FastAPI(Tittle='Healthcare-Management')
-
-app = FastAPI(title="Healthcare Management API")
-
-@app.get("/")
-def root():
-    return {"message": "Healthcare API is running"}
